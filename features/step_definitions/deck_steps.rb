aa69741cefd166e2da37b2cbd89052e601f2d521
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 03:37:35 -0400

    Playing with ranges

--- features/step_definitions/deck_steps.rb
+++ features/step_definitions/deck_steps.rb
@@ -1,5 +1,5 @@
-When /^I create a deck from (\d+) to (\d+)$/ do |arg1, arg2|
-    pending # express the regexp above with the code you wish you had
+When /^I create a deck from (\d+) to (\d+)$/ do |m, n|
+  Games::Parts::NumberDeck.new ((m.to_i)..(n.to_i))
 end
 
 Then /^I have (\d+) cards to deal$/ do |arg1|
--- lib/games/parts/deck.rb
+++ lib/games/parts/deck.rb
@@ -3,7 +3,16 @@ module Games
   module Parts
     
     class Deck  
+
+      
     end
 
+    class NumberDeck
+
+      def initialize(range)
+        require 'pry'
+        binding.pry
+      end
+    end
   end
 end
