ff34f0d203624ba477f955c46ed056bf24831b07
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 03:25:30 -0400

    Now pending

---
+++ features/step_definitions/deck_steps.rb
@@ -0,0 +1,8 @@
+When /^I create a deck from (\d+) to (\d+)$/ do |arg1, arg2|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^I have (\d+) cards to deal$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
