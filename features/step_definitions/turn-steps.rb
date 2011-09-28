6c3814aaf666a57214abd51186385dfc38625297
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 13:51:03 -0400

    Now pending

---
+++ features/step_definitions/turn-steps.rb
@@ -0,0 +1,8 @@
+Given /^A (\d+) turn game$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
+Then /^There should be (\d+) turns$/ do |arg1|
+    pending # express the regexp above with the code you wish you had
+end
+
