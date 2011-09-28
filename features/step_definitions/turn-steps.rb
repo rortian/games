744e754426324fd0ddb3258d560c18d8e2105f11
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 13:53:47 -0400

    Before doing inheritence with turns

--- features/step_definitions/turn-steps.rb
+++ features/step_definitions/turn-steps.rb
@@ -1,8 +1,12 @@
-Given /^A (\d+) turn game$/ do |arg1|
-    pending # express the regexp above with the code you wish you had
+Given /^A (\d+) turn game$/ do |n|
+  n = n.to_i
+  @game = Games::Parts::NTurns.new n 
 end
 
 Then /^There should be (\d+) turns$/ do |arg1|
+  times = 0
+  @game.each { times += 1 }
+  binding.pry
     pending # express the regexp above with the code you wish you had
 end
 
