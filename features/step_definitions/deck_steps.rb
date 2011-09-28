99006d5001b6577989e1edfa22088db5770736bc
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 03:52:58 -0400

    Passing trivial scenario

--- features/step_definitions/deck_steps.rb
+++ features/step_definitions/deck_steps.rb
@@ -1,8 +1,8 @@
 When /^I create a deck from (\d+) to (\d+)$/ do |m, n|
-  Games::Parts::NumberDeck.new ((m.to_i)..(n.to_i))
+  @deck = Games::Parts::NumberDeck.new ((m.to_i)..(n.to_i))
 end
 
-Then /^I have (\d+) cards to deal$/ do |arg1|
-    pending # express the regexp above with the code you wish you had
+Then /^I have (\d+) cards to deal$/ do |hack|
+  hack.to_i.should == @deck.size
 end
 
