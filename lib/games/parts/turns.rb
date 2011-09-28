3744a50a66f579fa7ddb9b0629f8588dc2a4408d
Jeremy Corbett <jeremy.lael@gmail.com>
2011-09-28 13:47:11 -0400

    Simple N turns

--- lib/games/parts/turns.rb
+++ lib/games/parts/turns.rb
@@ -1,7 +1,27 @@
 module Games
   module Parts
     class Turns
-      binding.pry
+
+      def each
+        unless over?
+          yield
+        end
+      end
+
+    end
+
+    class NTurns
+
+      def initialize(n)
+        @n = n
+        @left = n
+      end
+
+      def over?
+        @left -= 1
+        @left == 0
+      end
+
     end
   end
 end
